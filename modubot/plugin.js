935ba4e9fe63d2222e37e60e5a4462e12455bce6
KamalN7 <kamal@kamalnasser.net>
2013-07-29 12:44:24 +0300

    Formatting

--- modubot/plugin.js
+++ modubot/plugin.js
@@ -1,12 +1,12 @@
 var fs = require('fs');
 
 exports.reload = function (bot, namespace) {
-	exports.unload(bot, namespace);
-	exports.load(bot, namespace);
+    exports.unload(bot, namespace);
+    exports.load(bot, namespace);
 };
 
 exports.unload = function (bot, namespace) {
-	delete bot.plugins[namespace];
+    delete bot.plugins[namespace];
 };
 
 
@@ -17,40 +17,40 @@ exports.unload = function (bot, namespace) {
  * @param namespace
  */
 exports.load = function (bot, namespace) {
-	if (bot.debug) {
-		console.log("Loading Plugin: " + namespace);
-	}
+    if (bot.debug) {
+        console.log("Loading Plugin: " + namespace);
+    }
 
-	var name = namespace.split('/')[1];
+    var name = namespace.split('/')[1];
 
-	exports.unload(bot, namespace);
+    exports.unload(bot, namespace);
 
-	fs.readFile('./plugins/' + namespace + '/' + name + '.js', 'utf8', function (err, data) {
-		if (err) {
+    fs.readFile('./plugins/' + namespace + '/' + name + '.js', 'utf8', function (err, data) {
+        if (err) {
             console.log("Could not load " + namespace);
-			console.log(err);
-		} else {
-			eval(data);
+            console.log(err);
+        } else {
+            eval(data);
 
-			// Add plugin to active list
-			// "Plugin" is from the actual plugin
-			bot.plugins[namespace] = new Plugin(bot);
+            // Add plugin to active list
+            // "Plugin" is from the actual plugin
+            bot.plugins[namespace] = new Plugin(bot);
 
-			/*
-			 * Hooks
-			 */
-			['registered', 'motd', 'names', 'topic', 'join', 'part', 'quit', 'kick', 'kill', 'message', 'notice', 'ping', 'pm', 'ctcp', 'ctcpNotice', 'ctcpPrivmsg', 'ctcpVersion', 'nick', 'plusMode', 'minusMode', 'whois', 'channelistStart', 'channelistItem', 'channelList', 'raw', 'error'].forEach(function (event) {
-				var onEvent = 'on' + event.charAt(0).toUpperCase() + event.substr(1),
-					callback = bot.plugins[namespace][onEvent];
+            /*
+             * Hooks
+             */
+            ['registered', 'motd', 'names', 'topic', 'join', 'part', 'quit', 'kick', 'kill', 'message', 'notice', 'ping', 'pm', 'ctcp', 'ctcpNotice', 'ctcpPrivmsg', 'ctcpVersion', 'nick', 'plusMode', 'minusMode', 'whois', 'channelistStart', 'channelistItem', 'channelList', 'raw', 'error'].forEach(function (event) {
+                var onEvent = 'on' + event.charAt(0).toUpperCase() + event.substr(1),
+                    callback = bot.plugins[namespace][onEvent];
 
-				if (typeof callback == 'function') {
-					bot.client.addListener(event, callback);
+                if (typeof callback == 'function') {
+                    bot.client.addListener(event, callback);
                     bot.debug && console.log("Registered " + onEvent + " hook for " + namespace);
-				}
-			}, bot);
+                }
+            }, bot);
 
             bot.debug && console.log("Loaded " + namespace);
-		}
-	});
+        }
+    });
 
 };

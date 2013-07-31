ef93e8a977a8c8738c1fd3e51771766f35868167
clone1018 <luke@axxim.net>
2013-07-31 09:12:38 -0500

    Adding a more local config

--- modubot.js
+++ modubot.js
@@ -1,33 +1,41 @@
-var modu = require('./modubot/bot');
+var fs = require('fs');
 
-var config = {
-	host: "localhost",
-	port: 1234,
-	password: "",
-	nick: "Modubot",
-	username: "Modubot",
-	realname: "Modubot",
-	channels: ["#modubot"],
-	command: ".",
-	factoid: "?",
-	debug: true,
+/*
+ * Create the config files required
+ */
 
-	plugins: [
-		'axxim/factoids',
-		//'axxim/logger',
-		//'axxim/url',
-		//'kamaln7/echo'
-	],
+var defaultConfig = {
+    host: "irc.esper.net",
+    port: 6667,
+    password: "",
+    nick: "Modubot",
+    username: "Modubot",
+    realname: "Modubot",
+    channels: ["#modubot"],
+    command: ".",
+    factoid: "?",
+    debug: true,
 
-	database: {
-		host: "localhost",
-		user: "",
-		password: "",
-		database: ""
-	},
+    plugins: [
+        'axxim/factoids'
+    ],
 
-	admins: ["clone1018", "kamal"]
+    database: {
+        host: "localhost",
+        user: "",
+        password: "",
+        database: ""
+    },
+
+    admins: []
 };
+
+if(fs.existsSync('config/bot.json') === false) {
+    fs.writeFileSync('config/bot.json', JSON.stringify(defaultConfig, null, '\t'));
+}
+
+var config = require('./config/bot.json');
+var modu = require('./modubot/bot');
 
 var modubot = new modu.Bot(config);
 modubot.spawn();

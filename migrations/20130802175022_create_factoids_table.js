e81abb3a1f4da23ab7ff4b60ce07c39813811fec
KamalN7 <kamal@kamalnasser.net>
2013-08-03 00:24:09 +0300

    I won't edit any other migrations I promise

--- migrations/20130802175022_create_factoids_table.js
+++ migrations/20130802175022_create_factoids_table.js
@@ -1,13 +1,14 @@
 var create_factoids_table = new Migration({
 	up: function() {
         this.create_table('factoids', function(t) {
-            t.integer('id');
+            t.column('id', 'integer', {'auto_increment': true});
             t.string('factoid');
             t.text('content');
             t.string('owner');
             t.column('channel', 'string', {default_value: null})
             t.boolean('forgotten');
             t.boolean('locked');
+			t.column('created_at', 'timestamp', {'default': 'NOW()'});
             t.primary_key('id');
         });
 	},

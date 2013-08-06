57afe68a5ba4232c497adc3de23b22bab6c45855
KamalN7 <kamal@kamalnasser.net>
2013-08-06 16:55:52 +0300

    Escape `from` column

--- migrations/20130802175023_create_logging_table.js
+++ migrations/20130802175023_create_logging_table.js
@@ -3,7 +3,7 @@ var create_logging_table = new Migration({
 		this.create_table('logs', function (t) {
 			t.column('id', 'integer', {'auto_increment': true});
 			t.column('channel', 'string', {default_value: null});
-			t.string('from');
+			t.string('`from`');
 			t.string('mentions');
 			t.text('message');
 			t.column('created_at', 'timestamp', {'default': 'NOW()'});

98dc4e6e582afb3b87ff0e3d11bebb483a7b7dcd
KamalN7 <kamal@kamalnasser.net>
2013-09-22 11:25:55 +0300

    Youtube module

---
+++ plugins/kamaln7/youtube/youtube.ts
@@ -0,0 +1,58 @@
+var youtubeFeeds = require('youtube-feeds');
+var getYouTubeID = require('get-youtube-id');
+
+export class Plugin {
+	name:string;
+	title:string;
+	description:string;
+	version:string;
+	author:string;
+
+	bot:any;
+	database:any;
+	client:any;
+	commands:any;
+	regex:any;
+
+	constructor(bot:any) {
+		this.name = 'youtube';
+		this.title = 'Parse Youtube links and output information';
+		this.description = "Youtube module for Modubot";
+		this.version = '0.1';
+		this.author = 'Kamal Nasser';
+
+		this.bot = bot;
+		this.database = bot.database;
+		this.client = bot.client;
+		this.commands = {
+			'youtube': 'onCommandYoutube',
+			'yt': 'onCommandYoutube'
+		};
+		this.regex = /(?:https?:\/\/)?(?:(?:(?:www\.)?youtube\.com\/watch\?.*?v=([a-zA-Z0-9_\-]+))|(?:(?:www\.)?youtu\.be\/([a-zA-Z0-9_\-]+)))/i;
+	}
+
+	onCommandYoutube(from:string, to:string, message:string, args:any) {
+		var client = this.client;
+
+		if(args.length < 2){
+			this.client.reply(from, to, "Usage: .youtube http://youtube.com/watch?v=xyz");
+		}
+
+		var link = args[1];
+		var id = getYouTubeID(link);
+		if(id){
+			var plugin = this;
+			youtubeFeeds.video(id, function(err, data){
+				if(err){
+					plugin.bot.reply(from, to, err);
+					return;
+				}
+
+				plugin.bot.reply(from, to, data.title + " [" + data.uploader + "] " + data.viewCount + " views.");
+			});
+		} else {
+			this.bot.reply(from, to, "Invalid link.");
+		}
+	}
+
+}

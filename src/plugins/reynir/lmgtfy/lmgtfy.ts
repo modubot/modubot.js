66f86d8da2a27d44dc4810d98a3aaca8e839b513
Reynir Björnsson <reynir@reynir.dk>
2014-07-24 18:10:01 +0200

    LMGTFY plugin

---
+++ src/plugins/reynir/lmgtfy/lmgtfy.ts
@@ -0,0 +1,24 @@
+export class Plugin {
+  bot: any;
+  commands: any;
+
+  constructor(bot : any) {
+    this.bot = bot;
+    this.commands = {
+      'lmgtfy': 'onCommandLmgtfy'
+    };
+    this.querystring = require('querystring');
+  }
+
+  onCommandLmgtfy(from: string, to: string, message: string, args: Array<string>) {
+    if (args.length < 2) {
+      return this.usage(from, to);
+    }
+    var query = this.querystring.stringify({ q : args.splice(1).join(' ') });
+    this.bot.reply(from, to, 'http://lmgtfy.com/?'+query);
+  }
+
+  usage(from: string, to:string) {
+    this.bot.reply(from, to, 'Usage: .lmgtfy How is babby formed');
+  }
+}

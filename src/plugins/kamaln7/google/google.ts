54b36a3c2bb0fb55ab8c31cd14a64f485c2d9c5f
Kamal Nasser <hello@kamal.io>
2016-04-01 16:25:21 +0300

    fix kamaln7/google

--- src/plugins/kamaln7/google/google.ts
+++ src/plugins/kamaln7/google/google.ts
@@ -3,7 +3,7 @@ export class Plugin {
 	database:any;
 	client:any;
 	commands:any;
-	google:any;
+	request:any;
 
 	constructor(bot:any) {
 		this.bot = bot;
@@ -13,24 +13,36 @@ export class Plugin {
 			'google': 'onCommandGoogle',
 			'g': 'onCommandGoogle'
 		};
-		this.google = require('google');
-		this.google.resultsPerPage = 1;
+    this.request = require('request');
 	}
 
 	onCommandGoogle(from:string, to:string, message:string, args:any) {
 		var query = args.splice(1).join(' ');
-		this.google(query, (function(err, next, links){
-			if(!err && !links.length){
-				err = 'No results.';
-			}
+		var _this = this;
+    this.request({
+      url: 'http://ajax.googleapis.com/ajax/services/search/web',
+      qs: {v: '1.0', 'q': query}
+    }, function(err, response, body) {
+      if(err) {
+          _this.bot.reply(from, to, err, 'notice');
+          return;
+      }
 
-			if(err){
-				this.bot.reply(from, to, err, 'notice');
-				return;
-			}
+			if(response.statusCode == 200) {
+				try {
+					var results = JSON.parse(body);
 
-			this.bot.reply(from, to, links[0].title + ' - ' + links[0].link);
-		}).bind(this));
+					if(results.responseData && results.responseData.results && results.responseData.results.length > 0) {
+						var result = results.responseData.results[0];
+						_this.bot.reply(from, to, result.titleNoFormatting + ' - ' + result.url);
+					} else {
+						_this.bot.reply(from, to, 'no results.');
+					}
+				} catch (e) {
+					_this.bot.reply(from, to, 'error: ' + e.message, 'notice');
+				}
+			}
+    });
 	}
 
 }
